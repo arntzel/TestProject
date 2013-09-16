830d738877a46984237689a0f32a97ec282f884e
Eliot Arntz <arntzel@gmail.com>
2013-09-16 22:00:16 +0200

    Initial Commit

---
+++ TestProject/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  TestProject
+//
+//  Created by Eliot Arntz on 9/16/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestProject/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  TestProject
+//
+//  Created by Eliot Arntz on 9/16/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
